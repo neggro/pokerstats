4a654ccfb9c55e037d2e2af159d8a503355d0574
Gustavo Cañete <gustavoca245@gmail.com>
2017-01-06 14:51:20 -0300

    create modal component

---
+++ src/components/helpers/AppModal.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="modal">
+        <div class="modal-content">
+            <h4>{{title}}</h4>
+            <p>{{message}}</p>
+        </div>
+        <div class="modal-footer">
+            <a href="#" @click.prevent="dismissModal" class="modal-action modal-close waves-effect waves-green btn-flat">OK</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: [
+            'title',
+            'message'
+        ],
+
+        methods: {
+
+            dismissModal() {
+                this.$emit('onDismissModal');
+            }
+        }
+    }
+</script>
+
+<style lang="scss"></style>
