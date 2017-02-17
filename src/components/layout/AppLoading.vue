1d4ce5e40a825041b28dd1e2b9e3d00bc3d7c0c8
Gustavo Cañete <gustavoca245@gmail.com>
2017-02-17 17:59:03 -0300

    update indentation

--- src/components/layout/AppLoading.vue
+++ src/components/layout/AppLoading.vue
@@ -1,83 +1,83 @@
 <template>
-<div class="app-loading">
-    <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue">
-            <div class="circle-clipper left">
-                <div class="circle"></div>
+    <div class="app-loading">
+        <div class="preloader-wrapper big active">
+            <div class="spinner-layer spinner-blue">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
             </div>
-            <div class="gap-patch">
-                <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-                <div class="circle"></div>
-            </div>
-        </div>
 
-        <div class="spinner-layer spinner-red">
-            <div class="circle-clipper left">
-                <div class="circle"></div>
+            <div class="spinner-layer spinner-red">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
             </div>
-            <div class="gap-patch">
-                <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-                <div class="circle"></div>
-            </div>
-        </div>
 
-        <div class="spinner-layer spinner-yellow">
-            <div class="circle-clipper left">
-                <div class="circle"></div>
+            <div class="spinner-layer spinner-yellow">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
             </div>
-            <div class="gap-patch">
-                <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-                <div class="circle"></div>
-            </div>
-        </div>
 
-        <div class="spinner-layer spinner-green">
-            <div class="circle-clipper left">
-                <div class="circle"></div>
-            </div>
-            <div class="gap-patch">
-                <div class="circle"></div>
-            </div>
-            <div class="circle-clipper right">
-                <div class="circle"></div>
+            <div class="spinner-layer spinner-green">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
 
-export default {}
+    export default {}
 
 </script>
 
 <style lang="scss">
 
-.app-loading {
-    background: rgba(0, 0, 0, 0.35);
-    bottom: 0;
-    left: 0;
-    position: fixed;
-    right: 0;
-    top: 0;
-    z-index: 10;
-
-    .preloader-wrapper.big {
-        height: 96px;
+    .app-loading {
+        background: rgba(0, 0, 0, 0.35);
+        bottom: 0;
         left: 0;
-        margin: auto;
-        position: absolute;
+        position: fixed;
         right: 0;
-        top: 33%;
-        width: 96px;
+        top: 0;
+        z-index: 10;
+
+        .preloader-wrapper.big {
+            height: 96px;
+            left: 0;
+            margin: auto;
+            position: absolute;
+            right: 0;
+            top: 33%;
+            width: 96px;
+        }
     }
-}
 
 </style>
