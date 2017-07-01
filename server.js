99dbd43dfae4d909239b718371a8702db4dd2446
Gustavo Cañete <gustavoca245@gmail.com>
2017-07-01 16:56:46 -0300

    install and use compression for express js server, this brings gzip compression to files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var compress = require('compression');
 var favicon = require('serve-favicon');
 var path = require('path');
 
+app.use(compress());
 app.use(favicon(path.join(__dirname, '/src/assets/favicon', 'favicon.ico')));
 app.use('/src/assets', express.static(path.join(__dirname, '/src/assets')));
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
