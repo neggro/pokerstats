129e2e06776f88d4576b7a319a54d3a61c86fcb9
Gustavo Cañete <gustavoca245@gmail.com>
2017-07-03 15:27:08 -0300

    add middleware for express to be used with SPA and manage history API correctly

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var compress = require('compression');
+var history = require('connect-history-api-fallback');
 var favicon = require('serve-favicon');
 var path = require('path');
 
 app.use(compress());
+app.use(history());
 app.use(favicon(path.join(__dirname, '/src/assets/favicon', 'favicon.ico')));
 app.use('/src/assets', express.static(path.join(__dirname, '/src/assets')));
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
