7aa958fee6b8cea0318397852a9c9eaa3fd0af8d
Gustavo Cañete <gustavoca245@gmail.com>
2017-07-03 15:53:32 -0300

    update server, remove history middleware and redirect all urls to index.html

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
 var compress = require('compression');
-var history = require('connect-history-api-fallback');
 var favicon = require('serve-favicon');
 var path = require('path');
 
+var appPort = process.env.PORT || 3000;
+
 app.use(compress());
-app.use(history({
-    index: path.join(__dirname, '/index.html'),
-    verbose: true
-}));
+
 app.use(favicon(path.join(__dirname, '/src/assets/favicon', 'favicon.ico')));
 app.use('/src/assets', express.static(path.join(__dirname, '/src/assets')));
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
 app.use('/node_modules/materialize-css/fonts/roboto', express.static(path.join(__dirname, '/node_modules/materialize-css/fonts/roboto')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Starting app on port:', appPort);
+});
